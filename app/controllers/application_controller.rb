64ef1efcc6a5fb2ad7082e1f44f5ca400d3b707e
pofkuma <pof.kuma@gmail.com>
2021-12-17 20:06:13 +0900

    Add redirection
    ログイン・ログアウト後に表示するページを決める

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address biography])
   end
+
+  def signed_in_root_path(resource_or_scope)
+    user_path(resource_or_scope)
+  end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
