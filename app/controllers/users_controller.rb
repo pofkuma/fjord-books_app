b7016a94cf262be7ddb03ae08b39c7c65245c0cc
pofkuma <pof.kuma@gmail.com>
2021-12-17 16:04:16 +0900

    Add user views
    ユーザーの一覧画面と詳細画面を作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  # GET /users
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
+  # GET /users/1
+  def show; end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
+
+# frozen_string_literal: true
