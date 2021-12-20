768ecb7c162eaed2098d0191a612d8cc630ff073
pofkuma <pof.kuma@gmail.com>
2021-12-20 17:17:43 +0900

    Use letter_opener_web
    ブラウザ上でメールを確認できるようにしてみる

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,14 +76,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => "メールアドレス",
-    :password => "パスワード",
-    :authentication => 'login'
-  }
+  config.action_mailer.delivery_method = :letter_opener_web
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :books
   resources :users
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
