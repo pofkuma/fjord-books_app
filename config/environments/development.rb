9918672e35f896dea8a509ea8d30d63cf9893aaf
pofkuma <pof.kuma@gmail.com>
2021-12-20 15:27:08 +0900

    Add mailer
    パスワード再設定メールを経由してパスワードを再設定できるようにする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,15 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "メールアドレス",
+    :password => "パスワード",
+    :authentication => 'login'
+  }
 end
