e88f194d1a794755b2f167fc90ea9f935e0ce0a7
pofkuma <pof.kuma@gmail.com>
2022-01-15 06:58:47 +0900

    ユーザーフォローの関連設定
    フォローする／されるの関連を作成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :following_relationships,
+           class_name: 'Follow', foreign_key: 'follower_id',
+           dependent: :destroy, inverse_of: :follower
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :followed_relationships,
+           class_name: 'Follow', foreign_key: 'following_id',
+           dependent: :destroy, inverse_of: :following
+  has_many :followers, through: :followed_relationships, source: :follower
 end
