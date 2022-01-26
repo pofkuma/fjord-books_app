de5d731ad146e72443e45b1fd360cd8bd409a94c
pofkuma <pof.kuma@gmail.com>
2022-01-26 22:26:02 +0900

    htmlでのレスポンスのみのため respond_to は不要
    https://github.com/pofkuma/fjord-books_app/pull/5/files#r792132956

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,26 +4,22 @@ class FollowsController < ApplicationController
   def create
     follow = current_user.following_relationships.build(following_id: params[:user_id])
 
-    respond_to do |format|
-      if follow.save
-        format.html { redirect_to user_path(params[:user_id]), notice: t('.notice') }
-      elsif follow.errors.where(:following).any?
-        format.html { redirect_to user_path(params[:user_id]), alert: follow.errors.where(:following).last.full_message }
-      else
-        format.html { redirect_to user_path(params[:user_id]) }
-      end
+    if follow.save
+      redirect_to user_path(params[:user_id]), notice: t('.notice')
+    elsif follow.errors.where(:following).any?
+      redirect_to user_path(params[:user_id]), alert: follow.errors.where(:following).last.full_message
+    else
+      redirect_to user_path(params[:user_id])
     end
   end
 
   def destroy
     follow = current_user.following_relationships.find_by(following_id: params[:user_id])
 
-    respond_to do |format|
-      if follow.destroy
-        format.html { redirect_to user_path(params[:user_id]), notice: t('.notice') }
-      else
-        format.html { redirect_to user_path(params[:user_id]) }
-      end
+    if follow.destroy
+      redirect_to user_path(params[:user_id]), notice: t('.notice')
+    else
+      redirect_to user_path(params[:user_id])
     end
   end
 end
