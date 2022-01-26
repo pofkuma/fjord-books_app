6bf23413aad7df80d156cd35851b2946e2c7d659
pofkuma <pof.kuma@gmail.com>
2022-01-26 22:54:15 +0900

    エラー発生時は固定のアラートメッセージを表示させる
    https://github.com/pofkuma/fjord-books_app/pull/5/files#r792218473
    https://github.com/pofkuma/fjord-books_app/pull/5/files#r792297476

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -6,20 +6,18 @@ class FollowsController < ApplicationController
 
     if follow.save
       redirect_to user_path(params[:user_id]), notice: t('.notice')
-    elsif follow.errors.where(:following).any?
-      redirect_to user_path(params[:user_id]), alert: follow.errors.where(:following).last.full_message
     else
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(params[:user_id]), alert: t('.alert')
     end
   end
 
   def destroy
     follow = current_user.following_relationships.find_by(following_id: params[:user_id])
 
-    if follow.destroy
+    if follow&.destroy
       redirect_to user_path(params[:user_id]), notice: t('.notice')
     else
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(params[:user_id]), alert: t('.alert')
     end
   end
 end
