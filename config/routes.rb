29ad483b248129dc84c38a266b54aeeece1fa518
pofkuma <pof.kuma@gmail.com>
2022-02-01 23:17:07 +0900

    本の詳細画面にコメント投稿・確認機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books do
+    resources :comments, only: %i[create]
+  end
   resources :reports do
     resources :comments, only: %i[create]
   end
