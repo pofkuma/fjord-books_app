494c0f49996aea305191183f7dad532d399271ec
pofkuma <pof.kuma@gmail.com>
2022-02-13 19:51:46 +0900

    属性情報の取得処理をメソッド化
    投稿したユーザーの名前（未入力ならメアド）を返す処理をまとめる

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :description, :user_id, presence: true
+
+  def display_user_name
+    user.name.empty? ? user.email : user.name
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  def display_user_name
+    user.name.empty? ? user.email : user.name
+  end
 end
